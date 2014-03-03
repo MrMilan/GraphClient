4d46ecb74e16f53d58d053a5ddc5ffe4f2f270b1
Milan Polacek <polacek.emilan@gmail.com>
2014-03-03 13:15:11 +0100

    Add graphs

--- src/graphclient/GraphClient.java
+++ src/graphclient/GraphClient.java
@@ -3,20 +3,61 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package graphclient;
 
 /**
  *
  * @author User
  */
-public class GraphClient {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.GeneralPath;
+import javax.swing.*;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class GraphClient extends JPanel {
+
+    private static final int SIZE = 400;
+    private final GeneralPath path = new GeneralPath();
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(SIZE, SIZE);
     }
-    
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        double dt = Math.PI / 180;
+        int w = getWidth() / 2;
+        int h = getHeight() / 2;
+        path.reset();
+        path.moveTo(w, h);
+        for (double t = 0; t < 2 * Math.PI; t += dt) {
+            double x = w * Math.sin(5 * t) + w;
+            double y = h * Math.sin(4 * t) + h;
+            path.lineTo(x, y);
+        }
+        g2d.setColor(Color.blue);
+        g2d.draw(path);
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                JFrame f = new JFrame();
+                f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                f.add(new GraphClient());
+                f.pack();
+                f.setVisible(true);
+            }
+        });
+    }
 }
