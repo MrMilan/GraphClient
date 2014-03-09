4dd995ac7aa4f110e5f77a531e8413e67e53dc6d
Milan <Milan@10.0.0.33>
2014-03-09 12:19:36 +0100

    Pridani panelu a uprava vykresleni

--- src/graphclient/Jframe_vizu.java
+++ src/graphclient/Jframe_vizu.java
@@ -3,31 +3,32 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package graphclient;
 
 import java.awt.Graphics;
-import javax.swing.JFrame;
+import java.util.List;
 import javax.swing.JPanel;
 
 /**
  *
  * @author User
  */
-public class Jframe_vizu extends JPanel{
-    
-    short [] pole;
-    
-    public Jframe_vizu(short [] pole)
-    {
+public class Jframe_vizu extends JPanel {
+
+    List<Short> pole;
+
+    public Jframe_vizu(List<Short> pole) {
         super();
-        this.pole=pole;
-    
+        this.pole = pole;
+
     }
+
     @Override
-    public void paint(Graphics g)
-    {
-    g.drawLine(1,1, 100, 100);
+    public void paint(Graphics g) {
+        for (int i = 0; i < pole.size()-1; i++) {
+            g.drawLine(pole.get(i).intValue(), pole.get(i).intValue(), pole.get(i+1).intValue(), pole.get(i+1).intValue());
+        }
     }
-    
+
+
 }
--- src/graphclient/Visu_tester.java
+++ src/graphclient/Visu_tester.java
@@ -7,12 +7,11 @@
 package graphclient;
 
 
+import java.awt.BorderLayout;
 import java.io.DataInputStream;
 import java.io.EOFException;
 import java.io.IOException;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
 import javax.swing.JFrame;
 
 /**
@@ -23,11 +22,28 @@ public class Visu_tester {
     
     public static void main(String[] args)
     {
+        //Ziskani a vykresleni dat
         InputFacade readedData = Client();
-        Jframe_vizu panel = new Jframe_vizu(readedData);
-        JFrame f = new JFrame("Test");
-        f.add(panel);//pridani na 4 panely
-        //layout manager
+        Jframe_vizu panelA = new Jframe_vizu(readedData.canalA);
+//        Jframe_vizu panelB = new Jframe_vizu(readedData.canalB);
+//        Jframe_vizu panelC = new Jframe_vizu(readedData.canalC);
+//        Jframe_vizu panelD = new Jframe_vizu(readedData.canalD);
+//        
+        //prace s okenkem
+        JFrame f = new JFrame("Vykresleni dat");
+        f.setSize(800, 600);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setResizable(true);
+        
+        //nastavovani layoutu
+        BorderLayout b = new BorderLayout();
+        f.setLayout(b);
+        f.add(panelA,b.CENTER);
+//        f.add(panelB,b.NORTH);
+//        f.add(panelC,b.SOUTH);
+//        f.add(panelD,b.SOUTH);
+        
+        
         f.setVisible(true);
     }
     
